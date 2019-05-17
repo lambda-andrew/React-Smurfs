0054d94a83a988d7c3b43f780c49c9c53731e012
BrennanHolm <brennan.mholmich@gmail.com>
2019-05-17 13:08:32 -0500

    Basic functionality
    Does everything, but returns errors on posts that I didn't fix yet.

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import axios from 'axios'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,6 +13,28 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount = () => {
+    axios
+    .get("http://localhost:3333/smurfs")
+    .then((smurfs) => {
+      this.setState({smurfs: smurfs.data})
+    })
+    .catch((error) =>{
+      console.log('Looks like Gargamel took your smurf(s)!', error)
+    })
+  }
+
+  addSmurf = (smurf) => {
+    axios
+    .post(this.state.serverUrl, smurf)
+    .then((aSm) => {
+      this.setState({smurfs: aSm.data})
+    })
+    .catch((error) => {
+      console.log('Gargamel cast a spell to prevent more smurfs!', error);
+    })
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -19,6 +43,9 @@ class App extends Component {
       <div className="App">
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
+
+        <Route exact path="/" component = {Smurfs} />
+        <Route path="/smurf-form" component = {SmurfForm} />
       </div>
     );
   }
