2be80643e59b5d01179a26480bed655aa487d6bc
kamaboko1 <github65@hotmail.com>
2019-05-17 09:17:12 -0700

    installed styled-components. styled nav buttons

--- village/src/components/Navigation.js
+++ village/src/components/Navigation.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import Styled from 'styled-components';
 
 const Navigation = () => {
     return (
         <div>
-            <NavLink to="/smurf-form">Add A Smurf</NavLink>
-            <NavLink to="/">Smurf List</NavLink>
+            <NavLink to="/smurf-form"><Button>Add A Smurf</Button></NavLink>
+            <NavLink to="/"><Button>Smurf List</Button></NavLink>
         </div>
     )
 }
+
+const Button = Styled.button `
+    font-size: 1.2rem;
+    border-radius: 4px;
+    margin: 100px 5px 0 5px;
+    cursor: pointer;
+    :focus {
+        outline: none;
+    }
+`
 
 export default Navigation;
