941f35bb05576c5325dc9f09512fef33fb8d876d
David Barrios <barriosadavid@gmail.com>
2019-05-17 08:52:55 -0700

    axios get and post request are done. Smurfs list is displayed and form works with adding new smurfs

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -4,25 +4,35 @@ class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      newSmurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    this.props.add(this.state.newSmurf)
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      newSmurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
     });
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    e.persist();
+    this.setState(prevState => ({ 
+      newSmurf: {
+        ...prevState.newSmurf,
+        [e.target.name]: e.target.value 
+      }
+    }));
   };
 
   render() {
@@ -32,19 +42,19 @@ class SmurfForm extends Component {
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.newSmurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.newSmurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.newSmurf.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
